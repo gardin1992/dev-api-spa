1b8f922c733a97ca65d41979c628f5e0850fa4d8
Joao <gardin1992@gmail.com>
2016-10-08 16:55:54 -0300

    teste

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
+    response.json({
+        name: 'Financi api-spa'
+    });
 });
 
 app.listen(app.get('port'), function() {
