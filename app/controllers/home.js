e18959d17d715f04aec60af0bab07ec135a920c5
Joao <gardin1992@gmail.com>
2016-10-08 17:34:50 -0300

    create branch develpment

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,7 +7,7 @@
 exports.index = function (req, res) {
     if (req.params.format) { res.json({}); }
     else {
-        res.send('<h1>Financial Api</h1>'); //jade template
+        res.send('<h1>Development Financial Api</h1>'); //jade template
     }
     /*res.render('home/index', {
         title: 'Node Express Mongoose Boilerplate'
